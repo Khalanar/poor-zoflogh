3f12d5be1deba6faa88099a57d8e56a73149a1f6
psan <pablo.xez@gmail.com>
2021-08-27 12:07:26 +0100

    implement building buttons and show descriptions when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,37 @@
 let gameMessages = [
     "Oh no... an alien has crashed into Planet Earth.<br><br> Poor Zoflogh.",
-
 ]
 
+let buildingDescriptions = {
+    ship: "this is a supership",
+    generator: "this is a generator",
+    nursery: "this is a nursery",
+    printer: "this is a printer",
+    biopsy_room:"this is a supership",
+}
+
+
 document.addEventListener("DOMContentLoaded", start())
+
+function getBuildings(){
+    let buildings = document.getElementsByClassName("building");
+    
+    for (let building of buildings){
+        building.addEventListener("click", function(){
+            clickBuilding(building)
+        })
+        console.log(`${building.id} initialised`);
+    }
+}
+
+function clickBuilding(b){
+    console.log("clicked " + b.id)
+    showBuildingDescription(b)
+}
+
+function showBuildingDescription(b){
+    document.getElementById("building-description").innerText = buildingDescriptions[b.id]
+}
 
 function updateGameLog(message){
     let msgLog = document.getElementById("game-log").innerHTML
@@ -18,6 +46,7 @@ function updateGameLog(message){
 function start(){
     console.log("start")
     updateGameLog("beep")
+    getBuildings()
 }
 
 function update(){
