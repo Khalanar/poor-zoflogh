f7963d749c410b6889eebf33079eb41189b9a091
psan <pablo.xez@gmail.com>
2021-08-27 12:38:41 +0100

    add building descriptions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,11 @@ let gameMessages = [
 ]
 
 let buildingDescriptions = {
-    ship: "this is a supership",
-    generator: "this is a generator",
-    nursery: "this is a nursery",
-    printer: "this is a printer",
-    biopsy_room:"this is a supership",
+    ship: "Zoflogh's crashed ship.<br><br>Luckily some parts can be salvaged with some inventive and spare time.<br><br>You may need to access earthen materials to get out of there, build your way out of this planet.",
+    generator: "Solar Powered generator<br><br>Salvaged off some spare parts of Zoflogh's ship, this generator transforms photons emitted by the closest star into energy.<br><br>Can be upgraded for better energy output",
+    nursery: "Nursery and Incubator<br><br>Froongkians edited their genome for asexual reproduction, leisurly sex is however encouraged as a bonding exercise<br><br>Use DNA to lay eggs, and energy to incubate them.",
+    printer: "A recycler and 3D printer by Uglog Industries.<br><br>Insert any type of matter to be recycled into metamaterial, the only material used in planet Froongk. For walls and electronics to clothing, it is incredibly poisonous, do not ingest.",
+    biopsy_room:"Biopsy room<br><br>Start abduction missions to collect DNA samples from creatures around the planet.<br><br>Froongkian laws strongly advise against bonding with abductees, however it's not forbidden.",
 }
 
 
@@ -30,7 +30,7 @@ function clickBuilding(b){
 }
 
 function showBuildingDescription(b){
-    document.getElementById("building-description").innerText = buildingDescriptions[b.id]
+    document.getElementById("building-description").innerHTML = buildingDescriptions[b.id]
 }
 
 function updateGameLog(message){
