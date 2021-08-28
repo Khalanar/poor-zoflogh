dbb7c33efa0aae12f9534bc4631017a4147832db
psan <pablo.xez@gmail.com>
2021-08-28 12:29:35 +0100

    prototype building construction

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,15 @@ let gameMessages = [
 
 let buildingDescriptions = {
     ship: "Zoflogh's crashed ship.<br><br>Luckily some parts can be salvaged with some inventive and spare time.<br><br>You may need to access earthen materials to get out of here, build your way out of this planet.",
-    generator: "Solar Powered generator<br><br>Salvaged off some spare parts of Zoflogh's ship, this generator transforms photons emitted by the closest star into energy.<br><br>Can be upgraded for better energy output",
-    nursery: "Nursery and Incubator<br><br>Froongkians edited their genome for asexual reproduction, leisurly sex is however encouraged as a bonding exercise.<br><br>Use DNA to lay eggs, and energy to incubate them.",
-    printer: "A recycler and 3D printer by Uglog Industries.<br><br>Insert any type of matter to be recycled into metamaterial, the only material used in planet Froongk. For walls and electronics to clothing, it is incredibly poisonous, do not ingest.",
-    biopsy_room:"Biopsy room<br><br>Start abduction missions to collect DNA samples from creatures around the planet.<br><br>Froongkian laws strongly advise against bonding with abductees, however it's not forbidden.",
+    generator: "Solar Powered Generator.<br><br>Salvaged off some spare parts of Zoflogh's ship, this generator transforms photons emitted by the closest star into energy.<br><br>Can be upgraded for better energy output",
+    nursery: "Nursery and Incubator.<br><br>Froongkians edited their genome for asexual reproduction, leisurly sex is however encouraged as a bonding exercise.<br><br>Use DNA to lay eggs, and energy to incubate them.",
+    printer: "A Recycler and 3D Printer by Uglog Industries.<br><br>Insert any type of matter to be recycled into metamaterial, the only material used in planet Froongk. For walls and electronics to clothing, it is incredibly poisonous, do not ingest.",
+    biopsy_room:"Biopsy Room.<br><br>Start abduction missions to collect DNA samples from creatures around the planet.<br><br>Froongkian laws strongly advise against bonding with abductees, however it's not forbidden.",
 }
 
 let resources = {
     energy: 100,
-    metamaterials: 0,
+    metamaterials: 100,
     dna: 0,
 }
 
@@ -44,23 +44,28 @@ class Building{
                 console.log("Upgraded this building to " + this.level)
             }else{
                 let msg = `Not enough resources to upgrade the ${this.name}, poor Zoflogh`
-                gameMessages.unshift(msg)
-                updateGameLog()
-                console.log(msg)
+                // gameMessages.unshift(msg)
+                updateGameLog(msg)
             }
         }
     }
 }
 
-let printerUpgradeReq = [
+let printer = new Building("printer", 0, [
     {energy: 10, metamaterials: 10, dna:0}, //Upgrade from 0 to 1
     {energy: 50, metamaterials: 100, dna:0}, //Upgrade from 1 to 2
     {energy: 100, metamaterials: 200, dna:0}, //Upgrade from 2 to 3
     {energy: 200, metamaterials: 500, dna:0}, //Upgrade from 3 to 4
     {energy: 400, metamaterials: 1000, dna:0}, //Upgrade from 4 to 5
-]
+])
 
-let printer = new Building("printer", 0, printerUpgradeReq)
+let generator = new Building("printer", 0, [
+    {energy: 10, metamaterials: 10, dna:0}, //Upgrade from 0 to 1
+    {energy: 50, metamaterials: 100, dna:0}, //Upgrade from 1 to 2
+    {energy: 100, metamaterials: 200, dna:0}, //Upgrade from 2 to 3
+    {energy: 200, metamaterials: 500, dna:0}, //Upgrade from 3 to 4
+    {energy: 400, metamaterials: 1000, dna:0}, //Upgrade from 4 to 5
+])
 
 function getBuildings(){
     let buildings = document.getElementsByClassName("building");
@@ -75,7 +80,9 @@ function getBuildings(){
 
 function clickBuilding(b){
     console.log("clicked " + b.id)
+    clearUpgradeArea()
     showBuildingDescription(b)
+    showBuildingUpgrades(b)
     if (b.id == "printer"){
         printer.upgrade()
     }else{
@@ -83,12 +90,45 @@ function clickBuilding(b){
     }
 }
 
+function clearUpgradeArea(){
+    document.getElementById("building-description").innerHTML = ""
+    document.getElementById("building-upgrades").innerHTML = ""
+}
+
+function showBuildingUpgrades(b){
+    console.log("showBuildingUpgrades")
+    if (b.id == "ship"){
+        let upgradesHTML = `
+        <img id="generator-build" class="build-button" src="./assets/images/generator.svg" alt="">
+        <img id="printer-build" class="build-button" src="./assets/images/printer.svg" alt="">
+        <img id="nursery-build" class="build-button" src="./assets/images/nursery.svg" alt="">
+        <img id="biopsy_room-build" class="build-button" src="./assets/images/biopsy_room.svg" alt="">
+        `
+        document.getElementById("building-upgrades").innerHTML = upgradesHTML
+        document.getElementById("generator-build").addEventListener("click", function(){
+            if(generator.level == "0"){
+                document.getElementById("generator").classList.remove("disabled")
+                document.getElementById("generator").classList.add("enabled")
+                generator.upgrade()
+                updateGameLog(`You managed to salvage the ship's energy generator.<br>               
+                It's in a sorry state but it should get you up and running. 
+                You should be able to upgrade it eventually for Zoflogh to leave this planet.`)
+            }else{
+                updateGameLog("This building is already up and running. Open the building for more options")
+            }
+        })
+    }
+}
+
 function showBuildingDescription(b){
     document.getElementById("building-description").innerHTML = buildingDescriptions[b.id]
 }
 
-function updateGameLog(){
+function updateGameLog(m){
     let msgLog = ""
+    if (m){
+        gameMessages.unshift(m)
+    }
     index = 0
     for (let msg of gameMessages){
         let color = index == 0 ? "highlight" : "normal";
