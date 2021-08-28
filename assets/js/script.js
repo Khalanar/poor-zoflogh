96922bc964bb29b06dc993040408ac0252fb567e
psan <pablo.xez@gmail.com>
2021-08-28 11:44:37 +0100

    add resources and buildings to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,57 @@ let buildingDescriptions = {
     biopsy_room:"Biopsy room<br><br>Start abduction missions to collect DNA samples from creatures around the planet.<br><br>Froongkian laws strongly advise against bonding with abductees, however it's not forbidden.",
 }
 
+let resources = {
+    energy: 100,
+    metamaterials: 0,
+    dna: 0,
+}
 
 document.addEventListener("DOMContentLoaded", start())
+
+function resourceRefresh(){
+    document.getElementById("energy").innerText = resources.energy
+    document.getElementById("metamaterials").innerText = resources.metamaterials
+    document.getElementById("dna").innerText = resources.dna
+    console.log("Refreshed resources card")
+}
+
+class Building{
+    constructor(name, level, upgradeRequirements){
+        this.name = name;
+        this.level = level;
+        this.maxLevel = 5;
+        this.upgradeRequirements = upgradeRequirements;
+    }
+
+    upgrade(){
+        if (this.level < this.maxLevel){
+            let enoughEnergy = this.upgradeRequirements[this.level].energy <= resources.energy
+            let enoughMetamaterials = this.upgradeRequirements[this.level].metamaterials <= resources.metamaterials
+            let enoughDna = this.upgradeRequirements[this.level].dna <= resources.dna
+            let enoughResources = enoughEnergy && enoughMetamaterials && enoughDna
+            if (enoughResources){
+                this.level++;
+                console.log("Upgraded this building to " + this.level)
+            }else{
+                let msg = `Not enough resources to upgrade the ${this.name}, poor Zoflogh`
+                gameMessages.unshift(msg)
+                updateGameLog()
+                console.log(msg)
+            }
+        }
+    }
+}
+
+let printerUpgradeReq = [
+    {energy: 10, metamaterials: 10, dna:0}, //Upgrade from 0 to 1
+    {energy: 50, metamaterials: 100, dna:0}, //Upgrade from 1 to 2
+    {energy: 100, metamaterials: 200, dna:0}, //Upgrade from 2 to 3
+    {energy: 200, metamaterials: 500, dna:0}, //Upgrade from 3 to 4
+    {energy: 400, metamaterials: 1000, dna:0}, //Upgrade from 4 to 5
+]
+
+let printer = new Building("printer", 0, printerUpgradeReq)
 
 function getBuildings(){
     let buildings = document.getElementsByClassName("building");
@@ -27,14 +76,19 @@ function getBuildings(){
 function clickBuilding(b){
     console.log("clicked " + b.id)
     showBuildingDescription(b)
+    if (b.id == "printer"){
+        printer.upgrade()
+    }else{
+        console.log("this is not a printer, ignore")
+    }
 }
 
 function showBuildingDescription(b){
     document.getElementById("building-description").innerHTML = buildingDescriptions[b.id]
 }
 
-function updateGameLog(message){
-    let msgLog = document.getElementById("game-log").innerHTML
+function updateGameLog(){
+    let msgLog = ""
     for (let msg of gameMessages){
         msgLog += `
         <p>${msg}</p><hr>
@@ -45,8 +99,9 @@ function updateGameLog(message){
 
 function start(){
     console.log("start")
-    updateGameLog("beep")
+    updateGameLog()
     getBuildings()
+    resourceRefresh()
 }
 
 function update(){
